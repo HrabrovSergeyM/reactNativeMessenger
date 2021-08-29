eeef403d51e22bcbd5919d58de06dc97782890a1
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-29 18:25:06 +0300

    Update TabOneScreen.tsx

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,10 +1,48 @@
 import * as React from "react";
-import { Text, StyleSheet } from "react-native";
+import { View, StyleSheet, Image, Text } from "react-native";
 
 export default function TabOneScreen() {
-  return <Text style={styles.text}>Hello</Text>;
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+        }}
+      />
+      <View style={styles.rightContainer}>
+        <View style={styles.row}>
+          <Text style={styles.name}>Elon Musk</Text>
+          <Text style={styles.text}>11:11 AM</Text>
+        </View>
+        <Text style={styles.text}>Hola Hola Coca Cola</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  text: { fontSize: 30 },
+  container: {
+    flexDirection: "row",
+    padding: 10,
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  image: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: "bold",
+  },
+  text: {
+    color: "grey",
+  },
 });
