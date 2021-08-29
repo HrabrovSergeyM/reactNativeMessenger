2a74bb262155f237e856cb44a45ee8ad4e619cbd
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-29 20:10:36 +0300

    Update TabOneScreen.tsx

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,48 +1,15 @@
 import * as React from "react";
 import { View, StyleSheet, Image, Text } from "react-native";
+import ChatRoomItem from "../components/ChatRoomItem";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{
-          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
-        }}
-      />
-      <View style={styles.rightContainer}>
-        <View style={styles.row}>
-          <Text style={styles.name}>Elon Musk</Text>
-          <Text style={styles.text}>11:11 AM</Text>
-        </View>
-        <Text style={styles.text}>Hola Hola Coca Cola</Text>
-      </View>
+    <View style={styles.page}>
+      <ChatRoomItem />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    padding: 10,
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  row: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  image: {
-    height: 60,
-    width: 60,
-    borderRadius: 30,
-    marginRight: 10,
-  },
-  name: {
-    fontWeight: "bold",
-  },
-  text: {
-    color: "grey",
-  },
+  page: {},
 });
