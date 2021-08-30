b5fe47ef403e5f675fbe205f625dda236fc179be
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-30 17:00:21 +0300

    Update HomeScreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
-import { View, StyleSheet, Image, Text, FlatList } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  FlatList,
+  Pressable,
+} from "react-native";
+import { Auth } from "aws-amplify";
 import ChatRoomItem from "../components/ChatRoomItem";
 import chatRoomsData from "../assets/dummy-data/ChatRooms";
 
 export default function HomeScreen() {
+  const logOut = () => {
+    Auth.signOut();
+  };
   return (
     <View style={styles.page}>
       <FlatList
@@ -11,6 +22,18 @@ export default function HomeScreen() {
         data={chatRoomsData}
         renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
       />
+      <Pressable
+        onPress={logOut}
+        style={{
+          height: 50,
+          margin: 10,
+          borderRadius: 5,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text>Logout</Text>
+      </Pressable>
     </View>
   );
 }
