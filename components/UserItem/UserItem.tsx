dd9b531d7f85d724ea4f5fdd92aa3ca536d59929
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-31 15:59:34 +0300

    Update UserItem.tsx

--- components/UserItem/UserItem.tsx
+++ components/UserItem/UserItem.tsx
@@ -1,14 +1,19 @@
 import { useNavigation } from "@react-navigation/native";
+import { Auth, DataStore } from "aws-amplify";
 import React from "react";
 import { View, Image, Text, Pressable } from "react-native";
+import { ChatRoom, User } from "../../src/models";
 import styles from "./styles";
 
 export default function UserItem({ user }) {
-
   const navigation = useNavigation();
 
-  const onPress = () => {
-    navigation.navigate("ChatRoom", { id: chatRoom.id });
+  const onPress = async () => {
+    const newChatRoom = await DataStore.save(new ChatRoom({ newMessages: 0 }));
+
+    const authUser = await Auth.currentAuthenticatedUser();
+    const dbUser = await DataStore.query(User, authUser.attributes.sub);
+    console.log(dbUser);
   };
 
   return (
@@ -19,13 +24,10 @@ export default function UserItem({ user }) {
           uri: user.imageUri,
         }}
       />
-     
       <View style={styles.rightContainer}>
         <View style={styles.row}>
           <Text style={styles.name}>{user.name}</Text>
-          
         </View>
-      
       </View>
     </Pressable>
   );
