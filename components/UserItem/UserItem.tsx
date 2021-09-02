135992758e5adf3c6dbbdc79402d594f21eb2d52
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-09-02 04:22:34 +0300

    Update UserItem.tsx

--- components/UserItem/UserItem.tsx
+++ components/UserItem/UserItem.tsx
@@ -1,43 +1,39 @@
-import { useNavigation } from "@react-navigation/native";
-import { Auth, DataStore } from "aws-amplify";
-import React from "react";
-import { View, Image, Text, Pressable } from "react-native";
-import { ChatRoom, User, ChatRoomUser } from "../../src/models";
-import styles from "./styles";
+import React from 'react';
+import { Text, Image, View, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import styles from './styles';
+import { Auth, DataStore } from 'aws-amplify';
+import { ChatRoom, User, ChatRoomUser } from '../../src/models';
 
 export default function UserItem({ user }) {
   const navigation = useNavigation();
 
   const onPress = async () => {
-    const newChatRoom = await DataStore.save(new ChatRoom({ newMessages: 0 }));
 
+    // Create a chat room
+    const newChatRoom = await DataStore.save(new ChatRoom({newMessages: 0}));
+    
+    // connect authenticated user with the chat room
     const authUser = await Auth.currentAuthenticatedUser();
     const dbUser = await DataStore.query(User, authUser.attributes.sub);
+    await DataStore.save(new ChatRoomUser({
+      user: dbUser,
+      chatroom: newChatRoom
+    }))
 
-    await DataStore.save(
-      new ChatRoomUser({
-        user: dbUser,
-        chatroom: newChatRoom,
-      })
-    );
+    // connect clicked user with the chat room
+    await DataStore.save(new ChatRoomUser({
+      user,
+      chatroom: newChatRoom
+    }));
 
-    await DataStore.save(
-      new ChatRoomUser({
-        user,
-        chatroom: newChatRoom,
-      })
-    );
-    navigation.navigate("ChatRoom", { id: newChatRoom.id });
-  };
+    navigation.navigate('ChatRoom', { id: newChatRoom.id });
+  }
 
   return (
     <Pressable onPress={onPress} style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{
-          uri: user.imageUri,
-        }}
-      />
+      <Image source={{ uri: user.imageUri}} style={styles.image} />
+
       <View style={styles.rightContainer}>
         <View style={styles.row}>
           <Text style={styles.name}>{user.name}</Text>
