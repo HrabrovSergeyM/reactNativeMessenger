3b6a2dc34ee28371bd9fa70c5316dce92526eb76
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-09-02 04:22:41 +0300

    Update Message.tsx

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -1,29 +1,42 @@
-import { Auth, DataStore } from "aws-amplify";
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { DataStore } from "@aws-amplify/datastore";
 import { User } from "../../src/models";
+import { Auth, Storage } from "aws-amplify";
+import { S3Image } from "aws-amplify-react-native";
+import { useWindowDimensions } from "react-native";
+import AudioPlayer from "../AudioPlayer";
+
+const blue = "#3777f0";
+const grey = "lightgrey";
 
 const Message = ({ message }) => {
   const [user, setUser] = useState<User | undefined>();
   const [isMe, setIsMe] = useState<boolean>(false);
+  const [soundURI, setSoundURI] = useState<any>(null);
+
+  const { width } = useWindowDimensions();
 
   useEffect(() => {
     DataStore.query(User, message.userID).then(setUser);
   }, []);
+
+  useEffect(() => {
+    if (message.audio) {
+      Storage.get(message.audio).then(setSoundURI);
+    }
+  }, [message]);
+
   useEffect(() => {
     const checkIfMe = async () => {
       if (!user) {
         return;
       }
       const authUser = await Auth.currentAuthenticatedUser();
-      setIsMe(user.id == authUser.attributes.sub);
+      setIsMe(user.id === authUser.attributes.sub);
     };
     checkIfMe();
   }, [user]);
-
-  const myID = "u1";
 
   if (!user) {
     return <ActivityIndicator />;
@@ -34,9 +47,24 @@ const Message = ({ message }) => {
       style={[
         styles.container,
         isMe ? styles.rightContainer : styles.leftContainer,
+        { width: soundURI ? "75%" : "auto" },
       ]}
     >
-      <Text style={{ color: isMe ? "black" : "white" }}>{message.content}</Text>
+      {message.image && (
+        <View style={{ marginBottom: message.content ? 10 : 0 }}>
+          <S3Image
+            imgKey={message.image}
+            style={{ width: width * 0.7, aspectRatio: 4 / 3 }}
+            resizeMode="contain"
+          />
+        </View>
+      )}
+      {soundURI && <AudioPlayer soundURI={soundURI} />}
+      {!!message.content && (
+        <Text style={{ color: isMe ? "black" : "white" }}>
+          {message.content}
+        </Text>
+      )}
     </View>
   );
 };
@@ -49,12 +77,12 @@ const styles = StyleSheet.create({
     maxWidth: "75%",
   },
   leftContainer: {
-    backgroundColor: "#3872E9",
+    backgroundColor: blue,
     marginLeft: 10,
     marginRight: "auto",
   },
   rightContainer: {
-    backgroundColor: "lightgrey",
+    backgroundColor: grey,
     marginLeft: "auto",
     marginRight: 10,
   },
