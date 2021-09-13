988b2aaf85634af8101afabf852e662e7f33621d
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-09-13 19:39:01 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import Amplify, { Auth } from "aws-amplify";
+import Amplify, { Auth, DataStore, Hub } from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react-native";
 import config from "./src/aws-exports";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import { Message } from "./src/models";
 
 Amplify.configure(config);
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    const listener = Hub.listen("datastore", async (hubData) => {
+      const { event, data } = hubData.payload;
+      if (
+        event === "outboxMutationProcessed" &&
+        data.model === Message &&
+        !["DELIVERED", "READ"].includes(data.element.status)
+      ) {
+        DataStore.save(
+          Message.copyOf(data.element, (updated) => {
+            updated.status = "DELIVERED";
+          })
+        );
+      }
+    });
+
+    return () => listener();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
