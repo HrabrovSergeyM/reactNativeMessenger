4f13e76c5a5ea81abfa67f1f3ebfccf6436db8f1
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-09-13 23:51:43 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Amplify, { Auth, DataStore, Hub } from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react-native";
@@ -7,13 +7,15 @@ import config from "./src/aws-exports";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
-import { Message } from "./src/models";
+import { Message, User } from "./src/models";
+import moment from "moment";
 
 Amplify.configure(config);
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const listener = Hub.listen("datastore", async (hubData) => {
@@ -33,6 +35,49 @@ function App() {
 
     return () => listener();
   }, []);
+
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+    const subscription = DataStore.observe(User, user.id).subscribe((msg) => {
+      if (msg.model === User && msg.opType === "UPDATE") {
+        setUser(msg.element);
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, [user?.id]);
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      await updateLastOnline();
+    }, 1 * 60 * 1000);
+    return () => clearInterval(interval);
+  }, [user]);
+
+  const fetchUser = async () => {
+    const userData = await Auth.currentAuthenticatedUser();
+    const user = await DataStore.query(User, userData.attributes.sub);
+    if (user) {
+      setUser(user);
+    }
+  };
+
+  const updateLastOnline = async () => {
+    if (!user) {
+      return;
+    }
+    await DataStore.save(
+      User.copyOf(user, (updated) => {
+        updated.lastOnlineAt = +new Date();
+      })
+    );
+  };
 
   if (!isLoadingComplete) {
     return null;
